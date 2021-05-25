5c2eed4b9dfd5f03932d1e0ac580e3ca4a9f655b
asma <asma@1D-ASMA.DC-CAPES.COM>
2021-05-25 13:33:26 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import imageInSrc from './imageInSrc.jpg';
 import './App.css';
+import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    return ( <
+        div className = 'App' >
+        <
+        div >
+        <
+        div style = {
+            { border: 'solid 1px black', maxWidth: '100vw' }
+        } >
+        <
+        h1 className = "title red" > Asma < /h1>  <
+        img className = "image"
+        src = { imageInSrc }
+        alt = 'imagInSrc' /
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+        <
+        img className = "image"
+        src = "/imageInPublic.jpg" / >
+        <
+        /div> <
+        video width = { 320 }
+        height = { 240 }
+        controls >
+        <
+        source src = "myVideo.mp4"
+        type = "video/mp4" / >
+        <
+        /video> < /
+        div >
+
+        <
+        /div>
+    );
 }
 
 export default App;
